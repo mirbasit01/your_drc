9ff66dd25d4509136bc5116d6a16cab5bb1a99d6
mirbasit01 <mirt11477@gmail.com>
2025-07-10 14:04:36 +0500

    Add Tailwind CSS configuration with custom theme settings and plugins

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,11 +3,8 @@
 export default function Latout({ children} : Readonly <{ children : React.ReactNode}>) {
     return(
         <main className="font-work-sans">
-
             <Navbar/>
             {children}
-
-
         </main>
     )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
      <>
-     <h1>HOME</h1>
+     <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">HOME</h1>
      </>
   );
 }
