940f41159db7263d70b6c2ce7a5ae5000493620b
mirbasit01 <mirt11477@gmail.com>
2025-07-14 19:07:13 +0500

    live  server-only

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import StartupCard, { StartupTypeCard } from "@/componets/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { json } from "stream/consumers";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams, }: {
   searchParams: Promise<{ query?: string }>;
@@ -15,11 +16,11 @@ export default async function Home({ searchParams, }: {
   const session = await auth();
 
   // console.log(session?.id);
+// 
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
-  // const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
-
-   const posts = await client.fetch(STARTUPS_QUERY)
+  //  const posts = await client.fetch(STARTUPS_QUERY)
 
    console.log(JSON.stringify(posts , null , 2))
 
@@ -53,7 +54,7 @@ export default async function Home({ searchParams, }: {
           Pitch Your Startup, <br />
           Connect With Entrepreneurs
         </h1>
-        <p className="font-medium text-[20px] text-white max-w-2xl text-center break-words; !max-w-3xl">
+        <p className="font-medium text-[20px] text-white max-w-2xl text-center break-words;">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
@@ -77,6 +78,10 @@ export default async function Home({ searchParams, }: {
         </ul>
 
       </section>
+
+      <SanityLive/>
+
+
     </>
   );
 }
