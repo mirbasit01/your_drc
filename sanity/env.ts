f4b53eed31cc567b764f0f7ddd10a85ce641982c
mirbasit01 <mirt11477@gmail.com>
2025-07-15 15:08:24 +0500

    add

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,30 +1,3 @@
-// export const apiVersion =
-//   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-07-14'
-
-// export const dataset = assertValue(
-//   process.env.NEXT_PUBLIC_SANITY_DATASET,
-//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-// )
-
-// export const projectId = assertValue(
-//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-//   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-// )
-
-// function assertValue<T>(v: T | undefined, errorMessage: string): T {
-//   if (v === undefined) {
-//     throw new Error(errorMessage)
-//   }
-
-//   return v
-// }
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
-
-  return v
-}
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-07-14'
 
@@ -37,3 +10,11 @@ export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
