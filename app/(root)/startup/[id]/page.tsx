92a29a336a5c3ee55841517cb14ced80883bbeb1
mirbasit01 <mirt11477@gmail.com>
2025-07-15 14:44:58 +0500

    new build

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -2,13 +2,7 @@ import React from 'react'
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => { 
       const id = (await params).id;
-
-
-
-
-
       return <>
-
       <h1 className='text-black'> This is startup number id {id}</h1>
       </>
 
