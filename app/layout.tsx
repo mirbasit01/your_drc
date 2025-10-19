08d0993cfb902c422d451021dee6c831079cf297
Abdul Basit <121716860+mirbasit01@users.noreply.github.com>
2025-10-20 00:04:25 +0500

    Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,63 +1,22 @@
-
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
- import localFont from "next/font/local";
-// import { Toaster } from "@/components/ui/toaster";
-import 'easymde/dist/easymde.min.css';
+import "easymde/dist/easymde.min.css";
 import { Toaster } from "@/componets/ui/toaster";
-
+import Script from "next/script"; 
 const workSans = localFont({
   src: [
-    {
-      path: "./fonts/WorkSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Thin.ttf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-ExtraLight.ttf",
-      weight: "100",
-      style: "normal",
-    },
+    { path: "./fonts/WorkSans-Black.ttf", weight: "900", style: "normal" },
+    { path: "./fonts/WorkSans-ExtraBold.ttf", weight: "800", style: "normal" },
+    { path: "./fonts/WorkSans-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/WorkSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/WorkSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/WorkSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/WorkSans-Thin.ttf", weight: "200", style: "normal" },
+    { path: "./fonts/WorkSans-ExtraLight.ttf", weight: "100", style: "normal" },
   ],
   variable: "--font-work-sans",
 });
-
 
 export const metadata: Metadata = {
   title: "Your Drc",
@@ -71,6 +30,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+         <Script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="yK5T8xZNZXNX8XsMatQzcQ"
+          async
+        />
+      </head>
       <body className={workSans.variable}>
         {children}
         <Toaster />
